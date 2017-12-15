04d6686f31f2608d9086eee6eae2fd4a9270d5e5
Anastasiya Razumeyko <anastasiya.razumeyko@gmail.com>
2017-12-15 11:49:00 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(function(){
+   $('#contact-form input[type=submit]').click(sendForm); 
+});
+
+function sendForm(ev) {
+    const form  = document.getElementsByTagName('form')[0];
+    if (form.checkValidity()) {
+        ev.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/anastasiya.razumeyko@gmail.com", 
+            method: "POST",
+            data: {
+                name: $('#name').val(),
+                email: $('#email').val(),
+                feedback: $('#message').val()
+            },
+            dataType: "json"
+        })
+         .done( () => $('#thank-dialog').attr('open', 'open') )
+         .fail( () => $('#error-dialog').attr('open', 'open') );    
+    }
+}
