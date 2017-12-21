365922fbad4888f9f62738bd72539429dbd06e04
Anastasiya Razumeyko <anastasiya.razumeyko@gmail.com>
2017-12-21 17:16:18 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -21,3 +21,15 @@ function sendForm(ev) {
     }
 }
 
+$(function() {
+    $('.smooth').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
+
